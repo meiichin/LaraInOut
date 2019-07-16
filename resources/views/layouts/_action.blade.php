927f1c15b92d58c68e12c2ec4ecbec5b0d1ae439
Miichin <meiichan3017@gmail.com>
2019-07-16 16:53:14 +0700

    [Added] dynamic action and modal

---
+++ resources/views/layouts/_action.blade.php
@@ -0,0 +1,3 @@
+<a href="{{ $url_edit }}" class="modal-show edit" title="Edit {{ $model->nama }}"><i class="fa fa-pen text-inverse"></i></a> |
+<a href="{{ $url_destroy }}" class="btn-delete" title="{{ $model->nama }}"><i class="fa fa-trash text-danger"></i></a>
+
